226e734a28670a1b5f1fb40854e19e8648e7fb6f
KIM-SOLI <57980406+KIM-SOLI@users.noreply.github.com>
2020-02-19 21:32:06 +0900

    soli : 커맨드패턴
    스터디 2일차 커맨드패턴 공부

---
+++ SampleProject/Assets/SoliUnit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoliUnit : MonoBehaviour
+{
+    private SoliCommand buttonUp = new UpSoliCommand();
+    private SoliCommand buttonDown = new DownSoliCommand();
+    private SoliCommand buttonRight = new RightSoliCommand();
+    private SoliCommand buttonLeft = new LeftSoliCommand();
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            buttonUp.excute(this);
+        }
+        else if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            buttonDown.excute(this);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            buttonRight.excute(this);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            buttonLeft.excute(this);
+        }
+    }
+}
