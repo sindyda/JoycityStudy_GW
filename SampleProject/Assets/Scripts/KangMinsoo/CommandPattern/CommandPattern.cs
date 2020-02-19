a3a9017b069cc3ae4c1ef2163cc016a7f28184be
Minsoo <sindyda@gmail.com>
2020-02-19 20:52:29 +0900

    커멘트패턴_강민수
    1차 커밋

---
+++ SampleProject/Assets/Scripts/KangMinsoo/CommandPattern/CommandPattern.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandPattern
+{
+    public virtual void executeAction(int x, int y)
+    {
+    }
+}
+
+public class MoveUnitCommand : CommandPattern
+{
+    public int x, y;
+    public int undoX, undoY;
+    public Unit unit = null;
+
+    public MoveUnitCommand(Unit _unit, int _x, int _y)
+    {
+        x = _x;
+        y = _y;
+
+        unit = _unit;
+    }
+
+    public override void executeAction(int _x, int _y)
+    {
+        undoX = unit.posX;
+        undoY = unit.posY;
+        unit.MoveTo(x, y);
+    }
+}
+
