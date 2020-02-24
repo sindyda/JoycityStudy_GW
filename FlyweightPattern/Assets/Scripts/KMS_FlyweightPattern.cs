2e627041f98cb0fecc0b2221abae47fef950e8c5
Minsoo <sindyda@gmail.com>
2020-02-24 20:40:37 +0900

    경량패턴
    경량패턴 커밋

---
+++ FlyweightPattern/Assets/Scripts/KMS_FlyweightPattern.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class KMS_FlyweightPattern : MonoBehaviour
+{
+    const int mapSizeX = 16;
+    const int mapSizeY = 16;
+
+    KMS_FieldObject[,] fieldObjectArray = new KMS_FieldObject[mapSizeX, mapSizeY];
+
+    KMS_FieldObject fieldIsle = new KMS_FieldObject(2, 2, true, "Isle");
+    KMS_FieldObject fieldGuildDepot = new KMS_FieldObject(2, 2, false, "GuildDepot");
+    KMS_FieldObject fieldMonster = new KMS_FieldObject(3, 3, true, "Monster");
+
+    private void Start()
+    {
+        for (int i = 0; i < mapSizeX; ++i)
+        {
+            for (int j = 0; j < mapSizeY; ++j)
+            {
+                switch (Random.Range(0, 3))
+                {
+                    case 0:
+                        fieldObjectArray[i, j] = fieldIsle;
+                        break;
+                    case 1:
+                        fieldObjectArray[i, j] = fieldGuildDepot;
+                        break;
+                    case 2:
+                        fieldObjectArray[i, j] = fieldMonster;
+                        break;
+                }
+            }
+        }
+
+        for (int i = 0; i < mapSizeX; ++i)
+        {
+            for (int j = 0; j < mapSizeY; ++j)
+            {
+                GameObject obj = Instantiate(Resources.Load(fieldObjectArray[i, j].objectName, typeof(GameObject))) as GameObject;
+                obj.transform.localPosition = new Vector3(i, j, 0);
+            }
+        }
+    }
+}
