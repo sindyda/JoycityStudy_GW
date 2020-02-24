256f12d93a284a60683292f857379e6e37dc67da
KIM-SOLI <57980406+KIM-SOLI@users.noreply.github.com>
2020-02-24 20:45:10 +0900

    soli : flywreight pattern
    스터디3 : 경량패턴 스터디

---
+++ FlyweightPattern/Assets/FlyWeight_Soli/Terrain_Soli.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Terrain_Soli
+{
+    public Terrain_Soli(int movementCost, bool isWater, Texture texture)
+    {
+        movementCost_ = movementCost;
+        isWater_ = isWater;
+        texture_ = texture;
+    }
+
+    int getmovementCost() { return movementCost_; }
+    bool isWater() { return isWater_;}
+    Texture GetTexture() { return texture_; }
+
+    private int movementCost_;
+    private bool isWater_;
+    private Texture texture_;
+}
