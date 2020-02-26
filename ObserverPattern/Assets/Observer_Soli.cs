65d285f6eb7c9225dd04ca401f73e60372993ea9
KIM-SOLI <57980406+KIM-SOLI@users.noreply.github.com>
2020-02-26 21:02:12 +0900

    Soli : ObserverPattern
    스터디4일차 옵저버패턴

---
+++ ObserverPattern/Assets/Observer_Soli.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EventType_Soli
+{
+    EventType_Soli_A,
+    EventType_Soli_B,
+    EventType_Soli_C,
+}
+
+public class Observer_Soli
+{
+    public virtual void onNotify(Subject_Soli entity, EventType_Soli eventType)
+    { }
+}
+
+public class Achievement_Soli : Observer_Soli
+{
+    public override void onNotify(Subject_Soli entity, EventType_Soli eventType)
+    {
+        switch(eventType)
+        {
+            case EventType_Soli.EventType_Soli_A:
+                entity.achivementText.text = "A 달성";
+                break;
+            case EventType_Soli.EventType_Soli_B:
+                entity.achivementText.text = "B 달성";
+                break;
+            case EventType_Soli.EventType_Soli_C:
+                entity.achivementText.text = "C 달성";
+                break;
+        }
+    }
+}
+
+
+public class Audio_Soli : Observer_Soli
+{
+    public override void onNotify(Subject_Soli entity, EventType_Soli eventType)
+    {
+        switch (eventType)
+        {
+            case EventType_Soli.EventType_Soli_A:
+                entity.audioText.text = "A 아ㅏㅏㅏㅏㅏㅏㅏㅏㅏ";
+                break;
+            case EventType_Soli.EventType_Soli_B:
+                entity.audioText.text = "B 비ㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣ";
+                break;
+            case EventType_Soli.EventType_Soli_C:
+                entity.audioText.text = "C 씨ㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣ";
+                break;
+        }
+    }
+}
+
---
+++ ObserverPattern/Assets/Subject_Soli.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Subject_Soli : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        addObserver(new Audio_Soli());
+        addObserver(new Achievement_Soli());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (Input.inputString)
+        {
+            case "A":
+                notify(this, EventType_Soli.EventType_Soli_A);
+                break;
+            case "B":
+                notify(this, EventType_Soli.EventType_Soli_B);
+                break;
+            case "C":
+                notify(this, EventType_Soli.EventType_Soli_C);
+                break;
+        }
+    }
+
+    public Text achivementText;
+    public Text audioText;
+
+    List<Observer_Soli> observer_ = new List<Observer_Soli>();
+    
+    void notify(Subject_Soli entity, EventType_Soli eventType)
+    {
+        for(int i = 0; i < observer_.Count; ++i)
+        {
+            observer_[i].onNotify(entity, eventType);
+        }
+    }
+
+    void addObserver(Observer_Soli observer)
+    {
+        observer_.Add(observer);
+    }
+
+    void removeObserver(Observer_Soli observer)
+    {
+        observer_.Remove(observer);
+    }
+}
