cb96e814a196d4298d660fe966c2ee7d861f0325
JeonJaram <61148483+JeonJaram@users.noreply.github.com>
2020-02-26 20:38:49 +0900

    옵저버 패턴

---
+++ ObserverPattern/Assets/JJR_ObserverPattern.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum RockPaperScissors
+{
+    Rock = 0,
+    Paper = 1,
+    Scissors = 2,
+}
+
+public class JJR_ObserverPattern : MonoBehaviour
+{
+    public List<JJR_Observer> observers = new List<JJR_Observer>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Nofify(RockPaperScissors.Scissors);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Nofify(RockPaperScissors.Rock);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Nofify(RockPaperScissors.Paper);
+        }
+    }
+
+    void Nofify(RockPaperScissors type)
+    {
+        for (int i = 0; i < observers.Count; ++i)
+        {
+            var observer = observers[i];
+            if (observer != null)
+            {
+                observer.onNofity(type);
+            }
+        }
+    }
+}
+
