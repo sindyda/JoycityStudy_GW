9032404011dac503ba8eb33fee059401e6c06d40
KIM-SOLI <57980406+KIM-SOLI@users.noreply.github.com>
2020-03-04 20:59:25 +0900

    soli : prototypePattern
    스터디 5일차 : 프로토타입패턴

---
+++ ProtoTypePattern/Assets/Monster_Soli.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster_Soli
+{
+    public virtual Monster_Soli clone()
+    {
+        return new Monster_Soli();
+    }
+
+    public int health = 0;
+    public int speed = 0;
+}
+
+public class Ghost_Soli : Monster_Soli
+{
+    public Ghost_Soli(int health, int speed)
+    {
+        this.health = health;
+        this.speed = speed;
+    }
+
+    public override Monster_Soli clone()
+    {
+        return new Ghost_Soli(health, speed);
+    }
+}
+
+public class Demon_Soli : Monster_Soli
+{
+    public Demon_Soli(int health, int speed)
+    {
+        this.health = health;
+        this.speed = speed;
+    }
+
+    public override Monster_Soli clone()
+    {
+        return new Demon_Soli(health, speed);
+    }
+}
+
+
+public class Spawner_Soli
+{
+    public Spawner_Soli(Monster_Soli prototype)
+    {
+        prototype_ = prototype;
+    }
+
+    public Monster_Soli spawnMonster()
+    {
+        return prototype_.clone();
+    }
+
+    private Monster_Soli prototype_;
+}
---
+++ ProtoTypePattern/Assets/PrototypePatternTest_Soli.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrototypePatternTest_Soli : MonoBehaviour
+{
+    public int ghostNumber = 1;
+    public int demonNumber = 1;
+
+    Monster_Soli ghostPrototype = null;
+    Spawner_Soli ghostSpawner = null;
+
+    Monster_Soli demonPrototype = null;
+    Spawner_Soli demonSpawner = null;
+
+    void Start()
+    {
+        ghostPrototype = new Ghost_Soli(15, 3);
+        ghostSpawner = new Spawner_Soli(ghostPrototype);
+
+        demonPrototype = new Demon_Soli(40, 5);
+        demonSpawner = new Spawner_Soli(demonPrototype);
+
+        for (int i = 0; i < ghostNumber; ++i)
+        {
+            var tempGhost = ghostSpawner.spawnMonster();
+            Debug.Log(i + 1 + "번째 고슽 / 체력 : " + tempGhost.health + " /스피드 : " + tempGhost.speed);
+        }
+
+        for (int i = 0; i < demonNumber; ++i)
+        {
+            var tempDemon = demonSpawner.spawnMonster();
+            Debug.Log(i + 1 + "번째 악마? / 체력 : " + tempDemon.health + " /스피드 : " + tempDemon.speed);
+        }
+    }
+}
