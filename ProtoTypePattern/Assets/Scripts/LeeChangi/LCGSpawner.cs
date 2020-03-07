c9cfc6c291c931adb2b55bc85b72b94f07bf26da
changi lee <peartail2@gmail.com>
2020-03-07 16:32:30 +0900

    프로토타입해봄
    유니티 맞춰서 해봄 딴거 해볼게 있었는데 나중에 해봄

---
+++ ProtoTypePattern/Assets/Scripts/LeeChangi/LCGSpawner.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LeeChangiPrototypeTest
+{
+    public class LCGSpawner : MonoBehaviour
+    {
+        public Character1 Character1Base;
+        public Monster1 Monster1Base;
+        void SpawnMonster()
+        {
+            var spawnobj = Monster1Base?.Clone();
+            SetPosition(spawnobj);
+        }
+
+        
+        void SpawnCharacter()
+        {
+            var cloneObj = Spawner.SpawnObject<Character1>(Character1Base);
+            SetPosition(cloneObj);
+        }
+
+        private void SetPosition(in MonoBehaviour obj)
+        {
+            obj.transform.SetParent(transform, false);
+            obj.transform.localPosition = new Vector3(0, 0, 0);
+            obj.gameObject.SetActive(true);
+        }
+
+
+        private void Start()
+        {
+            SpawnCharacter();
+            SpawnMonster();
+        }
+    }
+
+
+    public class Spawner
+    {
+        public static T SpawnObject<T>(T baseObject) where T : MonoBehaviour
+        {
+            var result = GameObject.Instantiate(baseObject.gameObject);
+            if (result == null)
+            {
+                return default(T);
+            }
+            return result.GetComponent<T>();
+        }
+    }
+}
---
+++ ProtoTypePattern/Assets/Scripts/LeeChangi/PrototypeTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LeeChangiPrototypeTest
+{
+    public interface IPrototype
+    {
+
+    }
+
+
+    public class Prototype : IPrototype
+    {
+        private List<Prototype> prototype = new List<Prototype>(); 
+
+        public Prototype()
+        {
+
+        }
+    }
+    
+    public class SpawnTest
+    {
+
+    }
+
+
+}
+
