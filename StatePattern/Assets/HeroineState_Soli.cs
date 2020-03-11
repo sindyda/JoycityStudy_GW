2a671280d086c11f5d91064142806aa826cb50ae
KIM-SOLI <57980406+KIM-SOLI@users.noreply.github.com>
2020-03-11 22:05:58 +0900

    soli : StatePattern
    스터디 6일차 : 상태패턴

---
+++ StatePattern/Assets/HeroineState_Soli.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroineState_Soli
+{
+    public virtual HeroineState_Soli handleInput(Heroine_Soli heroine, KEY_TYPE input)
+    {
+        return null;
+    }
+
+    public virtual void update(Heroine_Soli heroine)
+    {
+
+    }
+}
+
+public class DuckingState_Soli : HeroineState_Soli
+{
+    const int MAX_CHARGE = 30;
+
+    public DuckingState_Soli()
+    {
+        chargeTime_ = 0;
+    }
+
+    public override HeroineState_Soli handleInput(Heroine_Soli heroine, KEY_TYPE input)
+    {
+        base.handleInput(heroine, input);
+
+        if(input == KEY_TYPE.RELEASE_DOWN)
+        {
+            heroine.setImage(HEROINE_IMAGE_TYPE.IMAGE_STANDING);
+            return new StandingState_Soli();
+        }
+
+        return null;
+    }
+
+    public override void update(Heroine_Soli heroine)
+    {
+        base.update(heroine);
+
+        chargeTime_++;
+        if (chargeTime_ > MAX_CHARGE)
+        {
+            chargeTime_ = 0;
+            heroine.superBomb();
+        }
+    }
+
+    private int chargeTime_;
+}
+
+public class StandingState_Soli : HeroineState_Soli
+{
+    public StandingState_Soli()
+    {
+
+    }
+
+    public override HeroineState_Soli handleInput(Heroine_Soli heroine, KEY_TYPE input)
+    {
+        base.handleInput(heroine, input);
+
+        if (input == KEY_TYPE.PRESS_DOWN)
+        {
+            heroine.setImage(HEROINE_IMAGE_TYPE.IMAGE_DUCKING);
+            return new DuckingState_Soli();
+        }
+        else if(input == KEY_TYPE.PRESS_B)
+        {
+            heroine.setImage(HEROINE_IMAGE_TYPE.IMAGE_JUMPING);
+            return new JumpingState_Soli();
+        }
+
+        return null;
+    }
+
+    public override void update(Heroine_Soli heroine)
+    {
+        base.update(heroine);
+    }
+}
+
+public class JumpingState_Soli : HeroineState_Soli
+{
+    const int MAX_JUMPING = 30;
+
+    public JumpingState_Soli()
+    {
+
+    }
+
+    public override HeroineState_Soli handleInput(Heroine_Soli heroine, KEY_TYPE input)
+    {
+        base.handleInput(heroine, input);
+
+        if (input == KEY_TYPE.PRESS_DOWN)
+        {
+            heroine.setImage(HEROINE_IMAGE_TYPE.IMAGE_DIVING);
+            return new DvingState_Soli();
+        }
+
+        return null;
+    }
+
+    public override void update(Heroine_Soli heroine)
+    {
+        base.update(heroine);
+
+        jumpTime_++;
+        if (jumpTime_ > MAX_JUMPING)
+        {
+            jumpTime_ = 0;
+            heroine.setStaningState();
+        }
+    }
+
+    private int jumpTime_;
+}
+
+public class DvingState_Soli : HeroineState_Soli
+{
+    const int MAX_DIVING = 40;
+
+    public DvingState_Soli()
+    {
+
+    }
+
+    public override HeroineState_Soli handleInput(Heroine_Soli heroine, KEY_TYPE input)
+    {
+        base.handleInput(heroine, input);
+
+        if (input == KEY_TYPE.PRESS_DOWN)
+        {
+            heroine.setImage(HEROINE_IMAGE_TYPE.IMAGE_STANDING);
+            return new StandingState_Soli();
+        }
+
+        return null;
+    }
+
+    public override void update(Heroine_Soli heroine)
+    {
+        base.update(heroine);
+
+        divingTime_++;
+        if (divingTime_ > MAX_DIVING)
+        {
+            divingTime_ = 0;
+            heroine.setStaningState();
+        }
+    }
+
+    private int divingTime_;
+}
---
+++ StatePattern/Assets/Heroine_Soli.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum HEROINE_IMAGE_TYPE
+{
+    IMAGE_STANDING,
+    IMAGE_JUMPING,
+    IMAGE_DUCKING,
+    IMAGE_DIVING
+}
+
+public enum KEY_TYPE
+{
+    RELEASE_DOWN,
+    PRESS_DOWN,
+    PRESS_B,
+}
+
+public class Heroine_Soli : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+            handleInput(KEY_TYPE.PRESS_DOWN);
+        else if (Input.GetKeyUp(KeyCode.DownArrow))
+            handleInput(KEY_TYPE.RELEASE_DOWN);
+        else if (Input.GetKeyUp(KeyCode.B))
+            handleInput(KEY_TYPE.PRESS_B);
+
+        state_.update(this);
+    }
+
+    private HeroineState_Soli state_ = new StandingState_Soli();
+
+    public void handleInput(KEY_TYPE input)
+    {
+        HeroineState_Soli state = state_.handleInput(this, input);
+        if (state != null)
+        {
+            state_ = state;
+        }
+    }
+    
+    public void setImage(HEROINE_IMAGE_TYPE imageType)
+    {
+        switch (imageType)
+        {
+            case HEROINE_IMAGE_TYPE.IMAGE_STANDING:
+                Debug.Log("서 있는 상태");
+                break;
+            case HEROINE_IMAGE_TYPE.IMAGE_JUMPING:
+                Debug.Log("점프 상태");
+                break;
+            case HEROINE_IMAGE_TYPE.IMAGE_DUCKING:
+                Debug.Log("엎드려 있는 상태");
+                break;
+            case HEROINE_IMAGE_TYPE.IMAGE_DIVING:
+                Debug.Log("내려찍기 상태");
+                break;
+        }
+    }
+
+    public void superBomb()
+    {
+        Debug.Log("기ㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣ");
+    }
+
+    public void setStaningState()
+    {
+        setImage(HEROINE_IMAGE_TYPE.IMAGE_STANDING);
+        state_ = new StandingState_Soli();
+    }
+}
