02adba82b086953d81fbf795459a3878722709f6
changi lee <peartail2@gmail.com>
2020-03-14 18:46:20 +0900

    리사이클러 해봄
    잘안됨

---
+++ ProtoTypePattern/Assets/Scripts/LeeChangi/RecyclePrefab.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LeeChangiPrototypeTest
+{
+    public class RecyclePrefab : MonoBehaviour
+    {
+        public int CloneCount;
+        public int MaxCount;
+
+        public void Clone()
+        {
+
+        }
+    }
+
+}
