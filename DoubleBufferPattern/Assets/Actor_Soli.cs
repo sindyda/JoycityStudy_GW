cd25551e7a445e3a481e0fbc076d6102245670ef
KIM-SOLI <57980406+KIM-SOLI@users.noreply.github.com>
2020-03-18 21:25:46 +0900

    soli : boublebufferpattern
    스터디 8일차 이중버퍼패턴

---
+++ DoubleBufferPattern/Assets/Actor_Soli.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor_Soli : MonoBehaviour
+{
+    public Actor_Soli()
+    {
+        currentSlapped_ = false;
+    }
+
+    public void update()
+    {
+        if (wasSlapped())
+        {
+            facing_.slap();
+        }
+    }
+
+    public void face(Actor_Soli actor)
+    {
+        facing_ = actor;
+    }
+
+    public void swap()
+    {
+        currentSlapped_ = nextSlapped_;
+        nextSlapped_ = false;
+    }
+   
+    public void slap()
+    {
+        Debug.Log(gameObject.name + "가 맞음");
+        nextSlapped_ = true;
+    }
+
+    public bool wasSlapped()
+    {
+        return currentSlapped_;
+    }
+
+    private Actor_Soli facing_;
+
+    private bool currentSlapped_;
+    private bool nextSlapped_;
+}
---
+++ DoubleBufferPattern/Assets/Stage_Soli.cs
@@ -0,0 +1,23 @@
+﻿public class Stage_Soli
+{
+    public void update()
+    {
+        for (int i = 0; i < actors.Length; ++i)
+        {
+            actors[i].update();
+        }
+
+        for (int i = 0; i < actors.Length; ++i)
+        {
+            actors[i].swap();
+        }
+    }
+
+    public void add(Actor_Soli actor, int index)
+    {
+        actors[index] = actor;
+    }
+
+    private const int ACTORS_NUM = 3;
+    private Actor_Soli[] actors = new Actor_Soli[ACTORS_NUM];
+}
