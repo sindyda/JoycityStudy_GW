b560ddce617e88267221ce7f2a223c00f8d1942b
changi lee <peartail2@gmail.com>
2020-03-21 15:04:25 +0900

    더블버퍼 해봄
    스레드안전 워커 만들어볼랬는데 잘안됨

---
+++ DoubleBufferPattern/Assets/Scripts/LeeChangi/LCGTester.cs
@@ -0,0 +1,55 @@
+﻿using LeeChangi;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class LCGTester : MonoBehaviour
+{
+    public ThreadSafeManager manager = new ThreadSafeManager();
+    void Start()
+    {
+       
+    }
+    int index = 0;
+    float updateTime = 0;
+    float firstupdatetime = 0;
+    float secondupdatetime = 0;
+    float thirdupdatetime = 0;
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+        //currentTime += Time.deltaTime;
+        
+
+        //updateTime += Time.deltaTime;
+        //for (int i = 0; i < 100; i++)
+        //{
+        //    index++;
+        //    ProcessWorker worker = new ProcessWorker(index, (long)currentTime + 2);
+        //    Thread t = new Thread(new ParameterizedThreadStart(AddWorker));
+        //    t.Start(worker);
+        //}
+
+        //if (updateTime > 1.0f)
+        //{
+        //    updateTime = 0;
+        //    manager.UpdateLogic((long)currentTime);
+
+            
+        //}
+        
+    }
+
+    private float currentTime = 0;
+    private void AddWorker(object randomValue)
+    {
+        if(manager != null)
+        {
+            manager.AddWorker((ProcessWorker)(randomValue));
+        }
+    }
+}
---
+++ DoubleBufferPattern/Assets/Scripts/LeeChangi/ThreadSafeManager.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+namespace LeeChangi
+{
+
+    public struct ProcessWorker
+    {
+        public long index { get; private set; }
+        public long CompleteTime { get; private set; }
+        public ProcessWorker(long i, long c)
+        {
+            index = i;
+            CompleteTime = c;
+        }
+        public bool IsComplete(long currentTime)
+        {
+            return CompleteTime < currentTime;
+        }
+
+        public void CompleteLogic()
+        {
+
+        }
+    }
+
+
+    public class InterLockedQueue<T>
+    {
+        private T[] buffers = null;
+        private int MaxSize = 0;
+        private int index = -1;
+        public int Index => index;
+        public InterLockedQueue(int size)
+        {
+            buffers = new T[size];
+            MaxSize = size;
+            index = -1;
+        }
+
+        public bool Push(T item)
+        {
+            if (MaxSize <= index)
+            {
+                Debug.LogError("Queue Is Full!");
+                //Push 실패 큐가 다 찼다.
+                return false;
+            }
+
+            int incValue = Interlocked.Increment(ref index);
+            buffers[incValue] = item;
+            return true;
+        }
+
+        public T Pop()
+        {
+            int decvalue = Interlocked.Decrement(ref index);
+            T result = buffers[decvalue + 1];
+            buffers[decvalue + 1] = default(T);
+            return result;
+        }
+
+        public IEnumerator<T> PopAll()
+        {
+            int count = index + 1;
+            for(int i =0;i<count;i++)
+            {
+                yield return Pop();
+            }
+        }
+    }
+
+    //실제로 되는지 안되는지는 확인을 하지 못했습니다.
+    //스위칭 타임에 동기화가 있는지도 확인이 필요합니다.
+    //그냥 만들어본겁니다.
+    public class ThreadSafeManager
+    {
+        private InterLockedQueue<ProcessWorker>[] pendingList = new InterLockedQueue<ProcessWorker>[2];
+        public ThreadSafeManager()
+        {
+            pendingList[0] = new InterLockedQueue<ProcessWorker>(10000);
+            pendingList[1] = new InterLockedQueue<ProcessWorker>(10000);
+        }
+        private int index = 0;
+        private void Switch()
+        {
+            index = 1 - index;
+        }
+        private List<ProcessWorker> workers = new List<ProcessWorker>();
+
+        public void AddWorker(ProcessWorker worker)
+        {
+            pendingList[index].Push(worker);
+        }
+
+        public void UpdateLogic(long currentTime)
+        {
+            Debug.Log($"Count : {workers.Count}||First:{pendingList[0].Index}/Second{pendingList[1].Index}");
+            int origin = index;
+            Switch();
+            var pending = pendingList[origin];
+            var iter = pending.PopAll();
+            
+            HashSet<long> duplicateChecker = new HashSet<long>();
+            while(iter.MoveNext())
+            {
+                if(duplicateChecker.Contains(iter.Current.index))
+                {
+                    Debug.LogError($"Error! {iter.Current.index}");
+                }
+                else
+                {
+                    duplicateChecker.Add(iter.Current.index);
+                }
+                workers.Add(iter.Current);
+            }
+
+
+            foreach (var worker in workers)
+            {
+                if (worker.IsComplete(currentTime))
+                {
+                    worker.CompleteLogic();
+
+                }
+            }
+            workers.RemoveAll(x => x.CompleteTime < currentTime);
+        }
+    }
+
+
+
+}
