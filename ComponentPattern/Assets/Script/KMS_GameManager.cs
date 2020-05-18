db0ef7e6143e74f48a61772491873dcb13c71574
Minsoo <sindyda@gmail.com>
2020-05-18 22:18:14 +0900

    Revert "Merge branch 'master' of https://github.com/sindyda/JoycityStudy_GW"
    This reverts commit 3998eb8b8c3bcefc737ac0461da90a45be1dedd3, reversing
    changes made to fc28cb64a1b9a9183ccdf520b35ffd1bc83305ac.

---
+++ ComponentPattern/Assets/Script/KMS_GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KMS_GameManager : MonoBehaviour
+{
+    GameObject playObject = null;
+    KMS_ComponentsPattern playCharactor = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playObject = GameObject.Instantiate(Resources.Load("Capsule") as GameObject);
+        if (playObject != null)
+        {
+            playCharactor = playObject.AddComponent<KMS_ComponentsPattern>();
+            if (playCharactor != null)
+            {
+                playCharactor.SetComponents(new KMS_ChearactorInputComponent(),
+                    new KMS_PlayerPhysicsComponent(), new KMS_PlayerGraphicsComponent());
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ ComponentPattern/Assets/Script/KMS_GraphicsComponent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KMS_GraphicsComponent
+{
+    public virtual void Update(KMS_ComponentsPattern obj)
+    {
+
+    }
+}
