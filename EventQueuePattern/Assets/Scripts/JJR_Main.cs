e63c1bb6894fac19d6d05f2b467a40d3120f1a0d
JeonJaram <61148483+JeonJaram@users.noreply.github.com>
2020-05-20 23:18:44 +0900

    event queue

---
+++ EventQueuePattern/Assets/Scripts/JJR_Main.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JJR_EventQueueManager
+{
+    private int headIndex = 0;
+    private int tailIndex = 0;
+
+    public void PushEvent(KeyCode KeyCode)
+    {
+        int inputNextIndex = (tailIndex + 1) % PENDING_MAX_LENGTH;
+        if (inputNextIndex == headIndex)
+        {
+            return;
+        }
+
+        pending[tailIndex] = KeyCode;
+
+        tailIndex = inputNextIndex;
+    }
+
+    public KeyCode PopEvent()
+    {
+        KeyCode returnValue = KeyCode.None;
+        if (headIndex == tailIndex)
+        {
+            return returnValue;
+        }
+
+        returnValue = pending[headIndex];
+        headIndex = (headIndex + 1) % PENDING_MAX_LENGTH;
+
+        return returnValue;
+    }
+
+    const int PENDING_MAX_LENGTH = 16;
+    KeyCode[] pending = new KeyCode[PENDING_MAX_LENGTH];
+};
+public class JJR_Main : MonoBehaviour
+{
+    JJR_EventQueueManager eventQueueManager = new JJR_EventQueueManager();
+    public JJR_Object obj = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            eventQueueManager.PushEvent(KeyCode.UpArrow);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            eventQueueManager.PushEvent(KeyCode.DownArrow);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            eventQueueManager.PushEvent(KeyCode.LeftArrow);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            eventQueueManager.PushEvent(KeyCode.RightArrow);
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (obj != null)
+            {
+                obj.InputKey(eventQueueManager.PopEvent());
+            }
+        }
+    }
+}
---
+++ EventQueuePattern/Assets/Scripts/JJR_Object.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JJR_Object : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void InputKey(KeyCode keyCode)
+    {
+        switch (keyCode)
+        {
+            case KeyCode.UpArrow:
+                transform.localPosition += new Vector3(0, 0.3f, 0);
+                break;
+            case KeyCode.DownArrow:
+                transform.localPosition += new Vector3(0, -0.3f, 0);
+                break;
+            case KeyCode.LeftArrow:
+                transform.localPosition += new Vector3(-0.3f, 0, 0);
+                break;
+            case KeyCode.RightArrow:
+                transform.localPosition += new Vector3(0.3f, 0, 0);
+                break;
+        }
+    }
+}
