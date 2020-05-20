c8b4dfcf5c8132547b85c0efd9cdea298e0c7aa0
Minsoo <sindyda@gmail.com>
2020-05-20 20:32:05 +0900

    이벤트 원형 큐 코드

---
+++ EventQueuePattern/Assets/Scripts/KMS_Audio.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct PlayMessage
+{
+    public int id;
+    public int volume;
+}
+
+public class KMS_Audio
+{
+    public const int MAX_PENDING = 10;
+
+    public int head = 0;
+    public int tail = 0;
+    public PlayMessage[] pending = new PlayMessage[MAX_PENDING];
+
+    public int currentID = 0;
+    public int currentVolume = 0;
+
+    public void PlaySound(int id, int volume)
+    {
+        if ((tail + 1) % MAX_PENDING != head)
+        {
+            pending[tail].id = id;
+            pending[tail].volume = volume;
+
+            tail = (tail + 1) % MAX_PENDING;
+            Debug.Log("이벤트 입력 : " + id + "," + volume);
+        }
+        else
+        {
+            Debug.LogError("Queue is full.");
+        }
+    }
+
+    public void StartEventQueue()
+    {
+        if (head == tail)
+        {
+            Debug.Log("출력할 사운드가 없습니다");
+            return;
+        }
+
+
+        Debug.Log(string.Format("{0}사운드를 {1}볼륨으로 플레이", pending[head].id, pending[head].volume));
+        head = (head + 1) % MAX_PENDING;
+    }
+}
--- EventQueuePattern/Assets/Scripts/KMS_EventQueuePattern.cs
+++ EventQueuePattern/Assets/Scripts/KMS_EventQueuePattern.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class KMS_EventQueuePattern : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    KMS_Audio kmsAudio = new KMS_Audio();
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            kmsAudio.PlaySound(kmsAudio.currentID, kmsAudio.currentVolume);
+        }
+        else if (Input.GetKeyDown(KeyCode.B))
+        {
+            kmsAudio.StartEventQueue();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            --kmsAudio.currentID;
+            if (kmsAudio.currentID < 0)
+                kmsAudio.currentID = 0;
+
+            Debug.Log("Current ID = " + kmsAudio.currentID);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            ++kmsAudio.currentID;
+
+            Debug.Log("Current ID = " + kmsAudio.currentID);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            kmsAudio.currentVolume += 10;
+
+            if (kmsAudio.currentVolume > 100)
+                kmsAudio.currentVolume = 0;
+
+            Debug.Log("Current Volume = " + kmsAudio.currentVolume);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            kmsAudio.currentVolume -= 10;
+
+            if (kmsAudio.currentVolume < 0)
+                kmsAudio.currentVolume = 0;
+
+            Debug.Log("Current Volume = " + kmsAudio.currentVolume);
+        }
     }
 }
