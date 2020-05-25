7cf06e207813919abca5f02f7fed8607af15d82c
KIM-SOLI <57980406+KIM-SOLI@users.noreply.github.com>
2020-05-25 19:10:40 +0900

    soli : EventQueue
    이벤트 큐

---
+++ EventQueuePattern/Assets/EventQueueManager_soli.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventQueueManager_soli : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Audio_soli.playSound(1, 100);
+        }
+        else if(Input.GetKeyDown(KeyCode.B))
+        {
+            Audio_soli.playSound(2, 70);
+        }
+        else if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Audio_soli.update();
+        }
+    }
+}
