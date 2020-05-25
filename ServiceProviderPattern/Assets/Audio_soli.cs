1405f9a61c0938d7f76efc61677ee64ad1a72ed2
KIM-SOLI <57980406+KIM-SOLI@users.noreply.github.com>
2020-05-25 20:57:53 +0900

    soli : serviceProviderPattern
    서비스 중개자 패턴

---
+++ ServiceProviderPattern/Assets/Audio_soli.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Locator
+{
+    private static Audio_soli service_;
+
+    public static Audio_soli GetAudio()
+    {
+        Debug.Assert(service_ != null);
+
+        return service_;
+    }
+
+    public static void provide(Audio_soli service)
+    {
+        service_ = service;
+    }
+}
+
+public class Audio_soli
+{
+    public virtual void playSound(int soundID)
+    {
+        
+    }
+
+    public virtual void stopSound(int soundID)
+    {
+
+    }
+
+    public virtual void stopAllSound()
+    {
+
+    }
+}
+
+public class ConsoleAudio_soli : Audio_soli
+{
+    public override void playSound(int soundID)
+    {
+        Debug.Log("ConsoleAudio " + soundID + " play");
+    }
+
+    public override void stopSound(int soundID)
+    {
+        Debug.Log("ConsoleAudio " + soundID + " stop");
+    }
+
+    public override void stopAllSound()
+    {
+        Debug.Log("ConsoleAudio stop all sound");
+    }
+}
+
+public class LoggedAudio_soli : Audio_soli
+{
+    private Audio_soli wrappde_;
+
+    public LoggedAudio_soli(Audio_soli wrappde)
+    {
+        wrappde_ = wrappde;
+    }
+
+    public override void playSound(int soundID)
+    {
+        Debug.Log("로그 사운드 출력");
+        wrappde_.playSound(soundID);
+    }
+
+    public override void stopSound(int soundID)
+    {
+        Debug.Log("로그 사운드 중지");
+        wrappde_.stopSound(soundID);
+    }
+
+    public override void stopAllSound()
+    {
+        Debug.Log("로그 모든 사운드 중지");
+        wrappde_.stopAllSound();
+    }
+}
