b1034dc3d1939116e4cce7ad26f1c4566bb2c080
Minsoo <sindyda@gmail.com>
2020-05-26 09:23:04 +0900

    작업 중

---
+++ ServiceProviderPattern/Assets/Scripts/KMS_Audio.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KMS_Audio
+{
+    public virtual void PlaySound(int soundID) { }
+    public virtual void StopSound(int soundId) { }
+    public virtual void StopAllSound() { }
+}
+
+public class ConsolAudio : KMS_Audio
+{
+
+}
+
--- ServiceProviderPattern/Assets/Scripts/KMS_Locator.cs
+++ ServiceProviderPattern/Assets/Scripts/KMS_Locator.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KMS_ServiceProviderPattern : MonoBehaviour
+public class KMS_Locator
 {
     
 }
