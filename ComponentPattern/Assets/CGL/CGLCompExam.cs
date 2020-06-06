dc3c19f15dcc55b13163d4b857423def54aa6f51
changi lee <peartail2@gmail.com>
2020-06-06 14:44:28 +0900

    컴포넌트 패턴
    컴포넌트패턴 해봄

---
+++ ComponentPattern/Assets/CGL/CGLCompExam.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CGL;
+
+public class CGLCompData : CGL.ComponentBase
+{
+    public CGLCompData(GameObject obj) 
+    {
+        gameObject = obj;
+    }
+}
+
+
+public class CGLCompExam : MonoBehaviour
+{
+    CGLCompData _compData = null;
+    private void Awake()
+    {
+        _compData = new CGLCompData(gameObject);
+        _compData.AddComponent<AudioComponent>();
+        _compData.AddComponent<GraphicRenderComponent>();
+
+    }
+
+    public void GraphicDraw()
+    {
+        var draw = _compData.GetComponent<GraphicRenderComponent>();
+        if (draw != null)
+        {
+            draw.RenderGraphic();
+        }
+    }
+
+    public void PlaySound()
+    {
+        var sound = _compData.GetComponent<AudioComponent>();
+        if (sound != null)
+        {
+            sound.PlaySound();
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ComponentPattern/Assets/CGL/ComponentBase.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace CGL
+{
+    public class ComponentBase
+    {
+        List<ComponentBase> components = new List<ComponentBase>();
+        protected GameObject gameObject = null; 
+        public ComponentBase()
+        {
+            
+        }
+
+        void AddGameObject(GameObject obj)
+        {
+            gameObject = obj;
+        }
+
+        public List<T> GetComponents<T>() where T : class
+        {
+            return components.FindAll((x) =>
+            {
+                return x.GetType() == typeof(T);
+            }) as List<T>;
+        }
+
+        public T GetComponent<T>() where T : class
+        {
+            return (components.Find((x) =>
+            {
+                return x.GetType() == typeof(T);
+            })) as T;
+        }
+
+        public void AddComponent<T>() where T : class, new()
+        {
+            var comp = new T() as ComponentBase;
+            comp.AddGameObject(gameObject);
+            components.Add(comp);
+        }
+    }
+
+
+    public class AudioComponent : ComponentBase
+    {
+        public AudioComponent() 
+        {
+        }
+
+        public void PlaySound()
+        {
+            Debug.Log("Sound Play!");
+        }
+    }
+
+
+    public class GraphicRenderComponent : ComponentBase
+    {
+        public GraphicRenderComponent() 
+        {
+        }
+
+        public void RenderGraphic()
+        {
+            Debug.Log("Draw Item!");
+        }
+    }
+
+
+}
+
