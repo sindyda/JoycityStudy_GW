934cabb0716d56cfbd3720d8ab6a86df0541c64d
changi lee <peartail2@gmail.com>
2020-06-07 13:55:55 +0900

    이벤트큐 해보기만 해봄
    일단 임시커밋용

---
+++ EventQueuePattern/Assets/CGL/CGLEventQueueSample.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CGL
+{
+
+    public class CGLEventQueueSample : MonoBehaviour
+    {
+        public CGLEventScheduler scheduler;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void Event1()
+        {
+            scheduler?.ForkEvent(new CGLEvent()
+            {
+                onEvent = () =>
+                {
+                    Debug.Log("Event1");
+                }
+            });
+        }
+
+        public void Event2()
+        {
+            scheduler?.ForkEvent(new CGLEvent()
+            {
+                onEvent = () =>
+                {
+                    Debug.Log("Event2");
+                }
+            });
+        }
+    }
+
+}
---
+++ EventQueuePattern/Assets/CGL/CGLEventScheduler.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CGL
+{
+    public struct CGLEvent
+    {
+
+        public Action onEvent;
+    }
+
+    public class CGLEventQueue
+    {
+        List<CGLEvent> queues1 = new List<CGLEvent>();
+        List<CGLEvent> queues2 = new List<CGLEvent>();
+
+        float remainTime = 1.0f;
+        bool isFirst = false;
+
+        public void AddEvent(CGLEvent ev)
+        {
+            if (isFirst)
+            {
+                queues1.Add(ev);
+            }
+            else
+            {
+                queues2.Add(ev);
+            }
+
+        }
+
+        public void update(float deltaTime)
+        {
+            remainTime -= deltaTime;
+            if(remainTime > 0)
+            {
+                return;
+            }
+
+            remainTime = 1f;
+
+            CGLEvent[] itemarray = null;
+            if (isFirst)
+            {
+                itemarray = new CGLEvent[queues2.Count];
+                queues2.CopyTo(itemarray);
+                queues2.Clear();
+            }
+            else
+            {
+                itemarray = new CGLEvent[queues1.Count];
+                queues1.CopyTo(itemarray);
+                queues1.Clear();
+            }
+
+            if(itemarray == null)
+            {
+                return;
+            }
+
+            int length = itemarray.Length;
+            for (int i = 0; i < length; i++)
+            {
+                itemarray[i].onEvent();
+            }
+
+            isFirst = !isFirst;
+        }
+    }
+
+    public class CGLEventScheduler : MonoBehaviour
+    {
+        CGLEventQueue eventQueue = new CGLEventQueue();
+
+        public void ForkEvent(CGLEvent sender)
+        {
+            eventQueue.AddEvent(sender);
+        }
+
+        private void Update()
+        {
+            float deltatime = Time.deltaTime;
+            eventQueue.update(deltatime);
+        }
+    }
+
+}
+
