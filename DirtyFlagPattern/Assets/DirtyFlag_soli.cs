33ad99e095941b74ae3bb8d15a1392bae98b0628
KIM-SOLI <57980406+KIM-SOLI@users.noreply.github.com>
2020-06-08 19:04:05 +0900

    soli : dirtyflag
    더티 플래그

---
+++ DirtyFlagPattern/Assets/DirtyFlag_soli.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirtyFlag_soli : MonoBehaviour
+{
+    void Start()
+    {
+        root?.addChild(node01);
+        root?.addChild(node02);
+
+        node01?.addChild(node02);
+    }
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            root?.render(new Vector3(0.1f,0,0), true);
+        }
+        else if(Input.GetKeyDown(KeyCode.B))
+        {
+            root?.render(new Vector3(-0.1f, 0, 0), true);
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            node01?.render(new Vector3(-0.1f, 0, 0), true);
+        }
+    }
+
+    public GraphNode_soli root;
+    public GraphNode_soli node01;
+    public GraphNode_soli node02;
+
+    private Vector3 pos = Vector3.zero;
+
+}
---
+++ DirtyFlagPattern/Assets/GraphNode_soli.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphNode_soli : MonoBehaviour
+{
+    private void Start()
+    {
+        world_ = transform.localPosition;
+        dirty_ = true;
+        childNode_ = new List<GraphNode_soli>();
+    }
+
+    public void addChild(GraphNode_soli child)
+    {
+        if(child)
+            childNode_.Add(child);
+    }
+
+    public void render(Vector3 pos, bool dirty)
+    {
+        dirty_ |= dirty;
+
+        if (dirty)
+        {
+            world_ = getWorldPosition(pos);
+            dirty_ = false;
+        }
+
+        changePosition(world_);
+
+        for (int i = 0; i < childNode_.Count; ++i)
+        {
+            childNode_[i].render(world_, dirty);
+        }
+    }
+
+    private Vector3 getWorldPosition(Vector3 pos)
+    {
+        return new Vector3(pos.x, 0, 0);
+    }
+
+    private void changePosition(Vector3 pos)
+    {
+        transform.localPosition += pos;
+    }
+
+    Vector3 world_;
+    private bool dirty_;
+    List<GraphNode_soli> childNode_;
+}
