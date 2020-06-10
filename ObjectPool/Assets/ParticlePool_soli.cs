fef6285e502de73c65cbff4f63e5c018e7770a8c
KIM-SOLI <57980406+KIM-SOLI@users.noreply.github.com>
2020-06-10 19:12:10 +0900

    soli : ObjectPool
    오브젝트풀

---
+++ ObjectPool/Assets/ParticlePool_soli.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlePool_soli : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        animate();
+
+        if(Input.GetKeyDown(KeyCode.N))
+        {
+            create(0, -10f, 0, 0.1f, 10);
+        }
+        else if (Input.GetKeyDown(KeyCode.M))
+        {
+            create(-10f, 0, 0.1f, 0, 10);
+        }
+    }
+
+    void create(float x_, float y_, float xVel_, float yVel_, float lifeTime_)
+    {
+        for (int i = 0; i < POOL_SIZE; ++i)
+        {
+            if (particles[i] == null || (particles[i] != null && !particles[i].inUse()))
+            {
+                particles[i] = Instantiate(particle, Vector3.zero, Quaternion.identity);
+                particles[i].init(x_, y_, xVel_, yVel_, lifeTime_);
+                return;
+            }
+        }
+    }
+
+    void animate()
+    {
+        for (int i = 0; i < POOL_SIZE; ++i)
+        {
+            if(particles[i] != null)
+                particles[i].animate();
+        }
+    }
+
+    public Particle_soli particle;
+    const int POOL_SIZE = 10;
+    Particle_soli[] particles = new Particle_soli[POOL_SIZE];
+}
---
+++ ObjectPool/Assets/Particle_soli.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle_soli : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    Particle_soli()
+    {
+        framesLeft = 0;
+    }
+
+    public void init(float x_, float y_, float xVel_, float yVel_, float lifeTime_)
+    {
+        x = x_;
+        y = y_;
+        xVel = xVel_;
+        yVel = yVel_;
+        framesLeft = lifeTime_;
+    }
+
+    public void animate()
+    {
+        if (!inUse())
+            return;
+        
+        framesLeft -= Time.deltaTime;
+        x += xVel;
+        y += yVel;
+
+        transform.localPosition = new Vector3(x, y, 0);
+    }
+
+    public bool inUse()
+    {
+        return framesLeft > 0;
+    }
+
+    private float framesLeft;
+    private float x, y;
+    private float xVel, yVel;
+}
